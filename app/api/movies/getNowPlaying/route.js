d604003be87f2b4c251137547f30415bf573fb7e
Jonathan Acosta <jonacosta86@gmail.com>
2024-11-06 22:07:53 -0500

    update route

--- app/api/movies/getNowPlaying/route.js
+++ app/api/movies/getNowPlaying/route.js
@@ -1,4 +1,3 @@
-import { unstable_noStore as noStore} from "next/cache";
 // get now playing movies
 export async function GET() {
 
@@ -8,12 +7,12 @@ export async function GET() {
         headers: {
             accept: 'application/json',
             Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhYmRhOTY1NGRlYmVhNDI2Y2UwMDg2MDQwYzBlNThmZiIsInN1YiI6IjY2NzE4YzQ4ZjNmODZjMGYwZDNmMGU4MSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.OqnjOW-CaBf_Rjq3q1Y55mJ-T-Tru9VlM2-rOyuJuTo'
-        }
+        },
+        cache:'no-store'
     };
 
     // run
     try {
-        noStore()
 
         // get now playing movies and return data to res variable
         const res = await fetch('https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1', options).then((data) => {
